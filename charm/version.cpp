99fda366216bdb7c24e2497db402a1e8c057a498
alanghof <andreas.langhoff@frm2.tum.de>
2020-07-22 10:41:52 +0200

    beginning of restructure to separate mesytec and charm devices

--- charm/version.cpp
+++
@@ -1,7 +0,0 @@
-const char* GIT_REV="2844426";
-const char* GIT_TAG="";
-const char* GIT_LATEST_TAG="v1";
-const char* GIT_DESCRIBE_TAGS="v1-9-g2844426";
-const char* GIT_NUMBER_OF_COMMITS_SINCE="9";
-const char* GIT_BRANCH="master";
-const char* GIT_DATE="Uncommmitted-2020-07-02T15_54";
