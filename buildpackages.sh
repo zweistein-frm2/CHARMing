cea9e40cfd1194b50062aa6e6c90e78b37976c91
alanghof <andreas.langhoff@frm2.tum.de>
2020-09-06 17:37:46 +0200

    docker need -m 4g (4 GB)  to build

--- buildpackages.sh
+++ buildpackages.sh
@@ -15,14 +15,14 @@ do
         printf "Filename: %s\n" "${i##*/}" # longest prefix removal
         printf "Extension: %s\n"  "${i##*.}"
 		ext=${i##*.}
-		
+
 		mkdir -p distribution/$ext
 		sudo chown root:root distribution/$ext
-		sudo docker build -f ${i##*/} -t charming-${i##*.} .
+		sudo docker build  -m 4g -f ${i##*/} -t charming-${i##*.} .
 		sudo docker create -ti --name dummy charming-${i##*.} bash
 		sudo docker cp dummy:/package distribution/${i##*.}
-		sudo docker rm -f dummy 
-		sudo find distribution -name '*.deb' -exec bash -c 'mv $0 "${0/Linux/'$ext'}" && cp "distribution/'$ext'/package/"*.deb distribution' {} \; 
+		sudo docker rm -f dummy
+		sudo find distribution -name '*.deb' -exec bash -c 'mv $0 "${0/Linux/'$ext'}" && cp "distribution/'$ext'/package/"*.deb distribution' {} \;
 		sudo chown $currentuser distribution/*.*
 		printf "\n\n"
     fi
