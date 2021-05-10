7811298cd983e3022e15c15842544ead3eefb8f0
alanghof <andreas.langhoff@frm2.tum.de>
2021-05-10 12:54:18 +0200

    debian11 support tentative

--- entangle-charming/Module.Globals.hpp
+++ entangle-charming/Module.Globals.hpp
@@ -15,7 +15,7 @@ EXTERN_FUNCDECLTYPE boost::thread_group worker_threads;
 
 Zweistein::Lock histogramsLock;
 std::vector<Histogram> histograms;
-static_assert(COUNTER_MONITOR_COUNT >= 4, "Mcpd8 Datapacket can use up to 4 counters: params[4][3]");
+static_assert(COUNTER_MONITOR_COUNT >= 4, "Mcpd8 Datapacket can use up to 4 counters");
 boost::atomic<unsigned long long> CounterMonitor[COUNTER_MONITOR_COUNT];
 
 boost::scoped_ptr<boost::asio::io_context> ptr_ctx(new boost::asio::io_context);
